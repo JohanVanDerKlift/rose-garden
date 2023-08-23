b2e3332aef8167240229d3ccbfe6591f39bb84d8
Klippie <info@johanvanderklift.nl>
2023-08-23 20:41:01 +0200

    Cleaned up data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,18 +2,17 @@ INSERT INTO authority (authority_name)
 VALUES ('ROLE_USER'), ('ROLE_ADMIN');
 
 INSERT INTO users (username, company_name, email, first_name, has_credit, last_name, password, phone_number)
-VALUES ('tester', 'testcompanyname', 'test@test.nl', 'testname', 'false', 'testlastname', '$2a$10$DkY.h9MDU5Wk4.DPbWCEzOPKSUrIdmE3dmAefKzWeUS9hQcct6.Ba', '0101234567'),
-       ('tester2', 'testcompanyname2', 'test2@test.nl', 'testname2', 'true', 'testlastname2', '$2a$10$YSjOxHQ2IeRiBN7TInSyjeRWT9gJapwdGhQyBt3eDmNlGrw0g7Etm', '0109876543');
+VALUES ('admin', 'admin', 'admin@admin.com', 'admin', 'false', 'admin', '$2y$10$dDTdrb7fdYZE/TX8L4i5nOWSK.eYgvIqpJG2gPkisADkJ8A6HkW5a', 'admin'),
+       ('tester', 'testcompanyname', 'test@test.nl', 'testname', 'false', 'testlastname', '$2a$10$DkY.h9MDU5Wk4.DPbWCEzOPKSUrIdmE3dmAefKzWeUS9hQcct6.Ba', '0101234567');
 
 INSERT INTO users_authorities (username, authority_name)
-VALUES  ('tester', 'ROLE_USER'),
-        ('tester2', 'ROLE_USER'),
-        ('tester', 'ROLE_ADMIN');
+VALUES  ('admin', 'ROLE_USER'),
+        ('admin', 'ROLE_ADMIN');
 
 INSERT INTO address (address_line_1, address_line_2, city, zipcode, username)
-VALUES ('Testerstreet 1', null, 'Testerton', '1234 AB', 'tester'),
-       ('Test-square 10', null, 'Testerton', '9876 BA', 'tester'),
-       ('Testerlane 2', 'Apartment A', 'Testcity', '4321 ZX', 'tester2');
+VALUES ('Testerstreet 1', null, 'Testerton', '1234 AB', 'admin'),
+       ('Test-square 10', null, 'Testerton', '9876 BA', 'admin'),
+       ('Testerlane 2', 'Apartment A', 'Testcity', '4321 ZX', 'tester');
 
 INSERT INTO product (id, tax, availability, name, description,  price)
 VALUES ('1', '9', 'true', 'Kleine loempia', '', '4.50'),
@@ -59,9 +58,3 @@ VALUES ('1', '9', 'true', 'Kleine loempia', '', '4.50'),
        ('201', '21', 'true', 'Coca cola', 'Blik 330ml', '2.50'),
        ('202', '21', 'true', 'Sisi orange', 'Blik 330ml', '2.50'),
        ('203', '21', 'true', 'Heineken', 'Blik 330ml (Alcohol houdend product 18+)', '3.50');
-
-INSERT INTO web_order (id, order_date_time, web_order_status, address_id, username)
-VALUES ('1', '2023-08-19T14:18:22.330552', 'PENDING', '1', 'tester');
-
-INSERT INTO web_order_detail (id, quantity, product_id, web_order_id)
-VALUES ('10', '2', '1', '1');
